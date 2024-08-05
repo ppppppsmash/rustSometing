4b5bfabcf2c67dfaf20c9626c29d6e3119a89a06
ppppppsmash <pppppp.work@gmail.com>
2024-08-05 22:21:51 +0900

    print、println、let

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    print!("Hello, ");
+    print!("Rust!");
+
+    println!("Hello, {}", "everyone");
+    println!("Hello, everyone");
+
+    let a: i32 = 1;
+    println!("{}", a);
+
+    let mut b: i32 = 2;
+    b = 10;
+    println!("{}", b);
+
+    let c: i32 = 1;
+
+    let d: i32;
+    d = 1;
+    println!("{}", d);
+
+    let e: &str = "hello";
+    println!("{}", e);
 }
