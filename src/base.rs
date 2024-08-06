3393edabfc45431b326983f9d49629e182037e55
ppppppsmash <pppppp.work@gmail.com>
2024-08-06 22:24:12 +0900

    update: type

---
+++ src/base.rs
@@ -0,0 +1,6 @@
+const B: i32 = 2;
+
+fn main() {
+  const A: i32 = 1;
+  println!("{}", B);
+}
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     b = 10;
     println!("{}", b);
 
-    let c: i32 = 1;
+    let cc: i32 = 1;
 
     let d: i32;
     d = 1;
@@ -21,4 +21,27 @@ fn main() {
 
     let e: &str = "hello";
     println!("{}", e);
+
+    let x: i32 = 1;
+    let y: f64 = 2.0;
+
+    let c: u16 = 3;
+
+    let n: f32 = 4.0f32;
+
+    let e: i32 = 1 + 2;
+    let e: i32 = 1 - 2;
+    let e: i32 = 1 * 2;
+    let e: i32 = 1 / 2;
+
+
+    let f: f64 = 1 as f64 + 2.0;
+
+    let g: bool = false;
+
+    println!("{}", g);
+
 }
+
+// Bはグローバルスコープ、どこからも参照できるようになる
+const B: i32 = 2;
